b747df7710f6980ac591e04d55df33e1fc9610a5
kranthigy <kranthi@goodyear.com>
2022-08-16 16:15:07 -0400

    loops

--- average1.py
+++ average1.py
@@ -1,6 +1,6 @@
 def main():
     n = eval(input("How many numbers do you have? "))
-    sum = 0
+    sum = 5
     for i in range(n):
         x = eval(input("Enter the number >> "))
         sum = sum + x
